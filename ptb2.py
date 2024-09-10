64217f7bbff5e36d8e0ed6bd6d82c50717f25a04
Thuan Do Laptop <thuando.contact@gmail.com>
2024-09-10 13:59:36 +0700

    done 2 file.py

---
+++ ptb2.py
@@ -0,0 +1,26 @@
+def ptb2(a, b, c):
+    if a == 0:
+        if b == 0:
+            if c == 0:
+                print("Phương trình vô số nghiệm")
+            else:
+                print("Phương trình vô nghiệm")
+        else:
+            print("Phương trình có nghiệm x = {:.4f}".format(-c/b))
+    if a != 0:
+        delta = b**2 - 4*a*c
+        if delta < 0:
+            print("Phương trình vô nghiệm")
+        elif delta == 0:
+            print("Phương trình có nghiệm kép x = {:.4f}".format(-b/(2*a)))
+        else:
+            print("Phương trình có 2 nghiệm phân biệt x1 = {:.4f} và x2 = {:.4f}".format((-b + delta**0.5)/(2*a), (-b - delta**0.5)/(2*a)))
+
+
+def main():
+    ptb2(-1,50,5)
+
+
+main()
+
+
