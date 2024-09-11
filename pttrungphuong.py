74baa1c1e2de7f6b4389c5912d01a8d124ed093f
Thuan Do PC <thuando.contact@gmail.com>
2024-09-11 16:16:38 +0700

    update from PC

--- pttrungphuong.py
+++ pttrungphuong.py
@@ -1,6 +1,6 @@
 
-# printd = print
-printd = lambda *argc,**argv:()
+printd = print
+# printd = lambda *argc,**argv:()
 
 def giaiptb2(a,b,c):
     import math
@@ -20,12 +20,12 @@ def giaiptb2(a,b,c):
             sn = 0
         elif delta == 0:
             sn = 1
-            x1 = -b/2*a
+            x1 = -b/(2*a)
         elif delta > 0:
             sn = 2
-            x1 = (-b+math.sqrt(delta))/2*a
-            x2 = (-b-math.sqrt(delta))/2*a
-            if x1 > x2: x1, x2 = x2, x1
+            x1 = (-b+math.sqrt(delta))/(2*a)
+            x2 = (-b-math.sqrt(delta))/(2*a)
+            if x1 > x2: x1,x2 = x2,x1
     return x1,x2,sn
 
 
@@ -36,59 +36,46 @@ def pttp(a,b,c):
 
 
     t1,t2,snt = giaiptb2(a,b,c)
-    printd(t1,t2,snt)
+    printd("t1 = {}, t2 = {}, snt = {}".format(t1,t2,snt))
 
     if snt == -1:
         sn = -1
     elif snt == 0:
         sn = 0
     elif snt == 1:
-        if t1 < 0:
-            sn = 0
-        elif t1 == 0:
-            sn = 1
-            x1 = 0
-        elif t1 > 0:
+        if t1 > 0:
             sn = 2
             x1 = -math.sqrt(t1)
             x2 = math.sqrt(t1)
-    elif snt == 1:
-        if t2 < 0:
-            sn = 0
-        elif t2 == 0:
+        if t1 == 0:
             sn = 1
             x1 = 0
+        elif t1 < 0:
+            sn = 0
         elif t2 > 0:
             sn = 2
             x1 = -math.sqrt(t2)
             x2 = math.sqrt(t2)
-    elif snt == 2:
-        if t1 < 0:
-            sn == 2
-            x1 = -math.sqrt(t2)
-            x2 = math.sqrt(t2)
-        elif t2 < 0:
-             sn == 2
-             x1 = -math.sqrt(t1)
-             x2 = math.sqrt(t1)
-    elif snt == 2:
-        if t1 == 0:
-            sn = 3
-            x1 = 0
-            x2 = -math.sqrt(t2)
-            x3 = math.sqrt(t2)
         elif t2 == 0:
-            sn = 3
+            sn = 1
             x1 = 0
-            x2 = -math.sqrt(t1)
-            x3 = math.sqrt(t1)       
+        else:
+            sn = 0
     elif snt == 2:
-        if t1 > 0 and t2 > 0:
+        if t1 == 0 and t2 > 0:
+            sn = 3
+            x1, x2, x3 = 0, -math.sqrt(t2), math.sqrt(t2)
+        elif t1 > 0 and t2 == 0:
+            sn = 3
+            x1, x2, x3 = 0, -math.sqrt(t1), math.sqrt(t1)
+        elif t1 < 0 and t2 > 0:
+            sn = 2
+            x1, x2 = -math.sqrt(t2), math.sqrt(t2)
+        elif t1 > 0 and t2 > 0:
             sn = 4
-            x1 = -math.sqrt(t1)
-            x2 = math.sqrt(t1)
-            x3 = -math.sqrt(t2)
-            x4 = math.sqrt(t2)
+            x1, x2 = -math.sqrt(t1), math.sqrt(t1)
+            x3, x4 = -math.sqrt(t2), math.sqrt(t2)
+
     return sn,x1,x2,x3,x4
 
 def main():
@@ -98,7 +85,7 @@ def main():
     printd(a,b,c)
 
     sn,x1,x2,x3,x4 = pttp(a,b,c)
-    printd(sn, x1,x2,x3,x4)
+    printd("sn = {}, x1 = {}, x2 = {}, x3 = {}, x4 = {}".format(sn, x1, x2, x3, x4))
 
     if sn == -1:
         print(f"Phương trình vô số nghiệm")
@@ -112,6 +99,6 @@ def main():
         print(f"Phương trình có 3 nghiệm x1 = {x1}; x2 = {x2}; x3 = {x3}")
     elif sn == 4:
         print(f"Phương trình có 4 nghiệm x1 = {x1}; x2 = {x2}; x3 = {x3}; x4 = {x4}")
-
+        
 if __name__ == "__main__":
     main()
